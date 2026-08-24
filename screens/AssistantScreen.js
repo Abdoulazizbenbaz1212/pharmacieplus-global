5403cf3628345751c0ae8bef9b7a41eb4ad78507
abdoulazizbenbaz1212 <abdoulazizbenbaz00@gmail.com>
2026-08-24 13:23:01 +0100

    Test: behavior height sur Android avec build natif resize

--- screens/AssistantScreen.js
+++ screens/AssistantScreen.js
@@ -70,7 +70,7 @@ export default function AssistantScreen() {
   return (
     <KeyboardAvoidingView
       style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
     >
       <FlatList
